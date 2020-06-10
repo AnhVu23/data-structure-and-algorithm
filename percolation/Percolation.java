3a48941778fc4b8558e5b9efab5d616c9f873daf
anhvu-95 <vu.haianh291@gmail.com>
2020-06-10 22:05:41 +0300

    Feat: Adding full class Percolation

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -1,131 +1,91 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation {
-    private int[] id;
-    private int neighbors[][];
+    private WeightedQuickUnionUF grid;
+    private WeightedQuickUnionUF full;
     private boolean[][] openGrid;
-    private int size;
-    private int[] sz;
-    private boolean isPercolate;
+    private final int size;
+    private final int gridSquared;
+    private int virtualBottom;
+    private int virtualTop;
+    private int openSites;
+
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
         if (n <= 0) {
             throw new IllegalArgumentException("number of sides should be a positive integer");
         }
-        id = new int[n];
-        sz = new int[n];
-        neighbors = new int[n][4];
         size = n;
+        gridSquared = n * n;
         openGrid = new boolean[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                var cellId = i * n + j;
-                id[cellId] = cellId;
-                sz[cellId] = 1;
-                var row = i + 1;
-                var col = j + 1;
-                var upperAdjacent = findAdjacent(row - 1, col);
-                var bottomAdjacent = findAdjacent(row + 1, col);
-                var leftAdjacent = findAdjacent(row, col - 1);
-                var rightAdjacent = findAdjacent(row, col + 1);
-                neighbors[cellId] = new int[]{upperAdjacent, bottomAdjacent, leftAdjacent, rightAdjacent};
-            }
-        }
+        grid = new WeightedQuickUnionUF(gridSquared + 2);
+        full = new WeightedQuickUnionUF(gridSquared + 1);
+        virtualTop = gridSquared;
+        virtualBottom = gridSquared + 1;
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
+        cellValid(row, col);
         boolean isOpen = isOpen(row, col);
-        int cellId = findCellId(row, col);
+        int cellId = findCellId(row - 1, col - 1);
         if (!isOpen) {
-            int upperAdjacent = neighbors[cellId][0];
-            int bottomAdjacent = neighbors[cellId][1];
-            int leftAdjacent = neighbors[cellId][2];
-            int rightAdjacent = neighbors[cellId][3];
-            if (upperAdjacent != -1) {
-                union(cellId, upperAdjacent);
-            }
-            if (bottomAdjacent != -1) {
-                union(cellId, bottomAdjacent);
-            }
-            if (leftAdjacent != -1) {
-                union(cellId, leftAdjacent);
-            }
-            if (rightAdjacent != -1) {
-                union(cellId, rightAdjacent);
+            openGrid[row - 1][col - 1] = true;
+            openSites++;
+            if (row == 1) {
+                grid.union(virtualTop, cellId);
+                full.union(virtualTop, cellId);
+            } else if (row == size) {
+                grid.union(virtualBottom, cellId);
+            } else {
+                int upperAdjacent = findAdjacent(row - 2, col - 1);
+                int bottomAdjacent = findAdjacent(row, col - 1);
+                int leftAdjacent = findAdjacent(row - 1, col - 2);
+                int rightAdjacent = findAdjacent(row - 1, col);
+                if (upperAdjacent != -1) {
+                    grid.union(cellId, upperAdjacent);
+                    full.union(cellId, upperAdjacent);
+                }
+                if (bottomAdjacent != -1) {
+                    grid.union(cellId, bottomAdjacent);
+                    full.union(cellId, bottomAdjacent);
+                }
+                if (leftAdjacent != -1) {
+                    grid.union(cellId, leftAdjacent);
+                    full.union(cellId, leftAdjacent);
+                }
+                if (rightAdjacent != -1) {
+                    grid.union(cellId, rightAdjacent);
+                    full.union(cellId, rightAdjacent);
+                }
             }
         }
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        return openGrid[row][col];
+        cellValid(row, col);
+        return openGrid[row - 1][col - 1];
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        boolean isFull = false;
-        int cellId = this.findCellId(row, col);
-        for (int i = 0; i < size; i++) {
-            if (id[cellId] == id[i]) {
-                isFull = true;
-                break;
-            }
-        }
-        return isFull;
+        cellValid(row, col);
+        return full.find(findCellId(row - 1, col - 1)) == grid.find(virtualTop);
     }
 
     // returns the number of open sites
     public int numberOfOpenSites() {
-        int counter = 0;
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if(openGrid[i][j]) {
-                    counter++;
-                }
-            }
-        }
-        return counter;
+        return openSites;
     }
 
     // does the system percolate?
     public boolean percolates() {
-        if (isPercolate) {
-            return true;
-        }
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                int bottomCellId = this.findCellId(size - 1, j);
-                if (id[i] == id[bottomCellId]) {
-                    isPercolate = true;
-                    break;
-                }
-            }
-        }
-        return isPercolate;
-    }
-
-    private void union(int i, int j) {
-        var rootI = findRoot(i);
-        var rootJ = findRoot(j);
-        if (rootI == rootJ) return;
-        if (sz[i] < sz[j]) {
-            id[i] = rootJ;
-            sz[j] += sz[i];
-        } else {
-            id[j] = rootI;
-            sz[i] += sz[j];
-        }
-    }
-
-    private int findRoot(int i) {
-        while (i != id[i]) {
-            id[i] = id[id[i]];
-            i = id[i];
-        }
-        return i;
+        return grid.find(virtualTop) == grid.find(virtualBottom);
     }
 
     private int findAdjacent(int row, int col) {
-        if (row - 1 < 0 || row - 1 > size || col - 1 < 0 || col - 1 > size) {
+        if (row < 0 || row >= size || col < 0 || col >= size) {
             return -1;
         } else {
             return findCellId(row, col);
@@ -133,11 +93,27 @@ public class Percolation {
     }
 
     private int findCellId(int row, int col) {
-        return (row - 1) * size + col - 1;
+        return row * size + col;
     }
 
-    // test client (optional)
-    public static void main(String[] args) {
+    private int cellValid(int row, int col) {
+        if (row < 1 || row >= size * size || col < 1 || col >= size * size) {
+            throw new IllegalArgumentException("");
+        }
+        return 1;
+    }
 
+    public static void main(String[] args) {
+        Percolation perc = new Percolation(5);
+        perc.open(1, 1);
+        System.out.println(perc.isOpen(1, 1));
+        System.out.println(perc.isFull(1, 1));
+        System.out.println(perc.isFull(2, 1));
+        System.out.println(perc.isOpen(4, 5));
+        perc.open(2, 2);
+        System.out.println(perc.percolates());
+        perc.open(2, 1);
+        System.out.println(perc.isFull(5, 1));
+        System.out.println(perc.percolates());
     }
 }
