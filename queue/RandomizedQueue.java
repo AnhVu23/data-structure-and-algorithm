ab8806c3fbbb060e2030bdb89d6c53d203829fd6
anhvu-95 <vu.haianh291@gmail.com>
2020-06-15 23:27:01 +0300

    Feat: Completing randomized queue

--- queue/RandomizedQueue.java
+++ queue/RandomizedQueue.java
@@ -1,6 +1,7 @@
 import edu.princeton.cs.algs4.StdRandom;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
     private Item[] array;
@@ -17,6 +18,13 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         return n == 0;
     }
 
+    // If empty, throw error
+    public void checkIfEmpty() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Array is empty");
+        }
+    }
+
     // return the number of items on the randomized queue
     public int size() {
         return n;
@@ -24,23 +32,59 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
     // add the item
     public void enqueue(Item item) {
-        int randomPos = StdRandom.uniform(n);
-
+        if (n == array.length) {
+            fixedCapacityStack(2 * array.length);
+        }
+        ;
+        array[n++] = item;
     }
 
     // remove and return a random item
     public Item dequeue() {
-
+        checkIfEmpty();
+        if (n == array.length / 4) {
+            fixedCapacityStack(array.length / 2);
+        }
+        int randomPos = StdRandom.nextInt(n);
+        Item item = array[randomPos];
+        array[randomPos] = array[--n];
+        array[n] = null;
+        return item;
     }
 
     // return a random item (but do not remove it)
     public Item sample() {
-
+        int randomPos = StdRandom.nextInt(n);
+        return array[randomPos];
     }
 
     // return an independent iterator over items in random order
     public Iterator<Item> iterator() {
+        return new ReverseArrayIterator();
+    }
 
+    public void fixedCapacityStack(int capacity) {
+        Item[] temp = (Item[]) new Object[capacity];
+        for (int i = 0; i < n; i++) {
+            temp[i] = array[i];
+        }
+        array = temp;
+    }
+
+    private class ReverseArrayIterator implements Iterator<Item> {
+        private int i = n;
+
+        public boolean hasNext() {
+            return i > 0;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        public Item next() {
+            return array[--i];
+        }
     }
 
     // unit testing (required)
